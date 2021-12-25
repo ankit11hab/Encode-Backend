0d031fe59b698e7ba29c02adea36106a3561b890
Ankit Guha <75986219+ankit11hab@users.noreply.github.com>
2021-12-25 21:51:35 +0530

    JWT Auth added

---
+++ authentication/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+@api_view(['GET'])
+def routes(request):
+    api_urls = {
+        'List': '/token',
+        'Create': '/token/refresh'
+    }
+    return Response(api_urls)
